e81ec121efb6094a8bde967d0a5d887cc6c06193
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2017-01-23 02:35:03 +0530

    Second step

--- headlines.py
+++ headlines.py
@@ -1,10 +1,22 @@
+import feedparser
 from flask import Flask
+
 
 app=Flask(__name__)
 
+ABSB_FEED="http://www.aftonbladet.se/sportbladet/rss.xml"
+
 @app.route("/")
 def get_news():
-    return "No news is good news"
+    feed=feedparser.parse(ABSB_FEED)
+    first_article=feed['entries'][0]
+    return """<html>
+      <body>
+        <h1> Sportbladet Headlines </h1>
+        <b>{0} </b><br/>
+        <i>{1] </i><br/>
+      </body>
+    </html>""".format(firstarticle.get("title"), firstarticle.get("pubDate")
 
 if __name__=='__main__':
     app.run(port=5000, debug=True)
